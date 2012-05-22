bfd62920111d8513cd40fd4f44b5bf2143bfb496
Yuen Lau <Yuen Lau>
2012-05-22 14:28:15 -0400

    Added Title Making

--- yog.php
+++ yog.php
@@ -2,66 +2,64 @@
 
 class yog {
 
-  private static $level = 0;
-  private static $array = array();
+  /*\
+  }o{ Configurations
+  \*/
+  
+  private static $keys  = array("TODO"    => "important",
+                                "DONE"    => "success", 
+                                "WAITING" => "warning",
+                                "CONTACT" => "warning",
+                                "NEXT"    => "info",  
+                                "STARTED" => "info",
+                                "CLOSED"  => "success");
+  /*\
+  }o{ Variable Declaration
+  \*/
+  
+  private static $level   = 0;
 
+  
+  /*\
+  }o{ Methods
+  \*/
   function yogThis($text) {
     $text = yog::parseSections($text);
     return $text;
   }
   
   
-  function mktitle($text)
+  private static function mktitle($matches)
   {
-    $actions  = '/^(\*+) (TODO|DONE|WAITING|CONTACT|NEXT|STARTED|CLOSED) (.*)/';
-    $tags     = "/(.*) (:.*):/s";
+    $text     = $matches[2];
+    $level    = strlen($matches[1]);
+    $rkeys    = '/^('.join("|",array_keys(yog::$keys)).') (.*)/';
+    $rtags     = '/(.*) (:.*):/s';
     
     $labels   = array();
     $labelstr = "";
     
-    if (preg_match($actions, $text, $matches))
+    if (preg_match($rkeys, $text, $matches))
     {
-      $text = $matches[2];
-      $label  = strtolower($matches[1]);
-      
-      switch($label)
-      {
-        case "todo":
-          $labelstr.= "<span class=\"label label-important\">$label</span> ";
-        break;
-        
-        case "done":
-        case "close":
-          $labelstr.= "<span class=\"label label-success\">$label</span> ";
-        break;
-        
-        case "waiting":
-        case "contact":
-          $labelstr.= "<span class=\"label label-warning\">$label</span> ";
-        break;
-        
-        case "next":
-        case "started":
-        default:
-          $labelstr.= "<span class=\"label label-info\">$label</span> ";
-        break;
-      }
+      $text     = $matches[2];
+      $label    = $matches[1];
+      $labelstr.= "<span class=\"label label-".yog::$keys[$label]."\">"
+                  .strtolower($label)."</span> ";
     }
     
-    if (preg_match($tags, $text, $matches))
+    if (preg_match($rtags, $text, $matches))
     {
       $text   = $matches[1];
       $labels = explode(":",substr($matches[2],1));
-      
       foreach($labels as $label)
         $labelstr.= "<span class=\"label\">$label</span> ";
     }
 
     if ($level == 1)
     {
-      return  $this->tl()."<div class=\"page-header\">\n".
+      return  "<div class=\"page-header\">\n".
               sprintf("<h%d>%s %s</h%d>\n", $level, trim($text), $labelstr, $level).
-              $this->tl()."</div>\n";
+              "</div>\n";
     }
     else
     {
@@ -69,9 +67,9 @@ class yog {
     }
   }
 
-  function parseSections($text)
+  public static function parseSections($text)
   {
-     return $text;   
+    return preg_replace_callback('/^(\*+?) (.*)/m', "yog::mktitle", $text);
   }
   
 }
